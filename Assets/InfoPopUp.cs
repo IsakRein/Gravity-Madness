0ae6604de69dceedf5ddae834ddc6e5a994d02d5
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2017-08-15 13:32:12 +0200

    123

---
+++ Assets/InfoPopUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPopUp : MonoBehaviour {
+
+	public void SendEmail ()
+	{
+	  	string email = "info@indigogames.se";
+	  	string subject = MyEscapeURL("Gravity Madness Contact");
+	  	string body = MyEscapeURL("Please enter your message here:\n________\n\n\n" +
+   		"________\n");
+  		//Open the Default Mail App
+  		Application.OpenURL ("mailto:" + email + "?subject=" + subject + "&body=" + body);
+ 	}  
+	 
+	string MyEscapeURL (string url)
+	{
+		return WWW.EscapeURL(url).Replace("+","%20");
+	}
+
+	public void Twitter() 
+	{
+		Application.OpenURL("https://twitter.com/IsakRein/");	
+	}
+}
