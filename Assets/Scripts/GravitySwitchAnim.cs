061939d21129a10f4b10a9b7bfb3de6a8df8a59e
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2017-08-28 07:53:37 +0200

    hej

--- Assets/Scripts/GravitySwitchAnim.cs
+++ Assets/Scripts/GravitySwitchAnim.cs
@@ -7,12 +7,31 @@ public class GravitySwitchAnim : MonoBehaviour {
 	private Animator GravityAnimator;
 	private SpriteRenderer SpriteR;
 	public string str;
-	
+
+	public string otherTrig;
+
+	public Animator otherAnim1;
+	public Animator otherAnim2;
+	public Animator otherAnim3;
+
 	void Start() 
 	{
 		Time.timeScale = 1; 
 		GravityAnimator = gameObject.GetComponent<Animator>();
 		SpriteR = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		if (otherAnim1.GetCurrentAnimatorStateInfo(0).IsName("GravitySwitch")) {
+			otherAnim1.SetTrigger(otherTrig);
+		}
+		if (otherAnim2.GetCurrentAnimatorStateInfo(0).IsName("GravitySwitch")) {
+			otherAnim2.SetTrigger(otherTrig);
+		}
+		if (otherAnim3.GetCurrentAnimatorStateInfo(0).IsName("GravitySwitch")) {
+			otherAnim3.SetTrigger(otherTrig);
+		}
 	}
 
 	public void Render() 
